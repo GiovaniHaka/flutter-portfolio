12402ae3a38feac4b447b14613c3da9cef663d34
GiovaniHaka <haka.giovani@gmail.com>
2022-11-08 16:53:15 -0300

    feat: UI components, constants and packages

---
+++ lib/common/constants/ui/custom_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+extension CustomColors on ColorScheme {
+  static const Color backgroundColor = Color(0xffFFFFFF);
+  static const Color pink = Color(0xffE82068);
+  static const Color pinkLight = Color(0xffFFE4EE);
+  static const Color greyLight = Color(0Xfff7f7f7);
+  static const Color greyMedium = Color(0xffDDDDDD);
+  static const Color greyDark = Color(0xff4E4E4E);
+  static const Color greenStrong = Color(0xff008b09);
+  static const Color green = Color(0xff45d300);
+  static const Color greenLight = Color(0xffd1f794);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ricky_and_morty/apps/characters/presentation/characters_list/characters_list_screen.dart';
+import 'package:ricky_and_morty/common/constants/ui/custom_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        backgroundColor: CustomColors.backgroundColor,
+        scaffoldBackgroundColor: CustomColors.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          color: CustomColors.backgroundColor,
+          actionsIconTheme: IconThemeData(
+            color: CustomColors.pink,
+          ),
+          elevation: 0,
+        ),
       ),
       home: const CharactersListScreen(),
     );
