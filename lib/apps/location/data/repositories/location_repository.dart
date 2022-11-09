c2910f7b9e1c3790edc862c7283acafac0cf4997
GiovaniHaka <haka.giovani@gmail.com>
2022-11-08 21:20:21 -0300

    feat: data/repositories layer

--- lib/apps/location/data/repositories/location_repository.dart
+++ lib/apps/location/data/repositories/location_repository.dart
@@ -1,7 +1,21 @@
 import 'package:dartz/dartz.dart';
+import 'package:ricky_and_morty/apps/location/data/sources/location_source.dart';
 import 'package:ricky_and_morty/apps/location/domain/models/location.dart';
 import 'package:ricky_and_morty/common/exceptions/failure.dart';
 
 abstract class LocationRepository {
   Future<Either<Failure, Location>> call(String url);
+}
+
+class LocationRepositoryImp implements LocationRepository {
+  late LocationSource _source;
+
+  @override
+  Future<Either<Failure, Location>> call(String url) {
+    try {
+      throw UnimplementedError();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 }
