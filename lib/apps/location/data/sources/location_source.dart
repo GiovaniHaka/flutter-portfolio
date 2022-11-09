cc7a3832b2d5ab8e1fff188467af4ed8a110c49f
GiovaniHaka <haka.giovani@gmail.com>
2022-11-08 21:23:32 -0300

    feat: data/sources layer

--- lib/apps/location/data/sources/location_source.dart
+++ lib/apps/location/data/sources/location_source.dart
@@ -1,6 +1,47 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
+import 'package:ricky_and_morty/common/constants/endpoints/ricky_morty_endpoints.dart';
 import 'package:ricky_and_morty/common/exceptions/failure.dart';
+import 'package:ricky_and_morty/services/http_client/domain/models/custom_request.dart';
+import 'package:ricky_and_morty/services/http_client/domain/models/custom_response.dart';
+import 'package:ricky_and_morty/services/http_client/domain/usecases/http_client.dart';
 
 abstract class LocationSource {
   Future<Either<Failure, Map<String, dynamic>>> call(String url);
+}
+
+class LocationSourceImp implements LocationSource {
+  late HttpClient _httpClient;
+
+  LocationSourceImp([HttpClient? httpClient]) {
+    _httpClient = httpClient ?? HttpClietImp();
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> call(String url) async {
+    try {
+      String path = RickMortyApi.locations;
+
+      final request = CustomRequest(path);
+
+      final response = await _httpClient.get(request);
+      final status = response.status;
+      final data = response.data;
+
+      switch (status) {
+        case ResponseStatus.success:
+          return Right(data!);
+        case ResponseStatus.unknown:
+          return Left(Failure('Não conseguimos encontrar a localização'));
+        case ResponseStatus.error:
+          return Left(Failure('Erro ao encontrar localização'));
+        default:
+          throw Exception();
+      }
+    } catch (e) {
+      log(e.toString());
+      return Left(Failure('Erro ao buscar localização'));
+    }
+  }
 }
